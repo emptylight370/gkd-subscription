c4e46809cbcd1c84377aedf582e76a7c590fa59c
Emptylight <1378990254@qq.com>
2025-04-03 00:28:05 +0800

    Simply Updates
    files(1): src/apps/com.dragon.read.ts

--- src/apps/com.dragon.read.ts
+++ src/apps/com.dragon.read.ts
@@ -7,8 +7,12 @@ export default defineGkdApp({
     {
       key: 1,
       name: '关闭领取金币窗口',
+      enable: true,
       rules: [
         {
+          matchTime: 3000,
+          actionMaximum: 1,
+          fastQuery: true,
           action: 'clickCenter',
           activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
           matches:
@@ -19,12 +23,61 @@ export default defineGkdApp({
     {
       key: 2,
       name: '关闭宝箱窗口',
+      enable: true,
       rules: [
         {
+          matchTime: 3000,
+          actionMaximum: 1,
+          fastQuery: true,
           action: 'clickCenter',
           activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
           matches:
             'FlattenUIText[text*="宝箱"][text*="奖励"] +44 LynxFlattenUI',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '点击签到按钮',
+      enable: true,
+      rules: [
+        {
+          matchTime: 3000,
+          actionMaximum: 1,
+          fastQuery: true,
+          action: 'clickCenter',
+          activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
+          matches: 'LynxFlattenUI[text^="立即签到"]',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '关闭签到完成窗口',
+      enable: true,
+      rules: [
+        {
+          matchTime: 10000,
+          actionMaximum: 1,
+          fastQuery: true,
+          action: 'clickCenter',
+          activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
+          matches: 'FlattenUIText[text*="看视频"] +6 LynxFlattenUI',
+        },
+      ],
+    },
+    {
+      key: 5,
+      name: '签到完成继续观看视频',
+      enable: false,
+      rules: [
+        {
+          matchTime: 10000,
+          actionMaximum: 1,
+          fastQuery: true,
+          action: 'clickCenter',
+          activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
+          matches: 'FlattenUIText[text*="看视频"]',
         },
       ],
     },
