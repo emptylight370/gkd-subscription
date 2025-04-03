a62ca9d262a0cd3145cc7fdde82e70a7ef9569a3
Emptylight <1378990254@qq.com>
2025-04-03 12:41:53 +0800

    从Alsouler那里抄了自动更新说明文档

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,7 @@
 import { updateDist } from '@gkd-kit/tools';
 import subscription from './check';
+import { updateReadme } from './updateReadme';
 
 await updateDist(subscription);
+
+await updateReadme();
