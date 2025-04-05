d76f4b67b3c3c5bae31e09809876a05f2b08288f
Emptylight <1378990254@qq.com>
2025-04-05 11:19:16 +0800

    feat: Simply Updates
    files(2): src/apps/com.zjzy.calendartime.ts, src/subscription.ts

--- src/apps/com.zjzy.calendartime.ts
+++ src/apps/com.zjzy.calendartime.ts
@@ -6,8 +6,8 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '功能弹窗-桌面组件打卡',
-      desc: '打卡之后点击完成',
+      name: '功能弹窗-打卡',
+      desc: '打卡、补卡之后点击完成',
       matchDelay: 700,
       rules: [
         {
@@ -18,6 +18,16 @@ export default defineGkdApp({
           activityIds:
             'com.zjzy.calendartime.desktop_widget.ui.WidgetTargetTransActivity',
           matches: 'TextView[text="打卡成功"] +3 Button[text="完成"]',
+        },
+        {
+          fastQuery: true,
+          matchTime: 5000,
+          action: 'clickCenter',
+          activityIds: 'com.zjzy.calendartime.ui.main.MainActivity',
+          anyMatches: [
+            '[vid="tv_card_tile"][text="补卡成功"] +3 [vid="btn_share"][text="完成"]',
+            '[vid="tv_card_tile"][text="打卡成功"] +3 [vid="btn_share"][text="完成"]',
+          ],
         },
       ],
     },
