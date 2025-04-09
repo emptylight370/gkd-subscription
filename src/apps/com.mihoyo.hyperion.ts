ea9cfd5904342bedaa804983b3292508ccdb21f5
Emptylight <1378990254@qq.com>
2025-04-09 19:14:06 +0800

    feat: Simply Updates
    files(1): src/apps/com.mihoyo.hyperion.ts

--- src/apps/com.mihoyo.hyperion.ts
+++ src/apps/com.mihoyo.hyperion.ts
@@ -24,14 +24,16 @@ export default defineGkdApp({
     {
       key: 2,
       name: '分段功能-领取签到奖励',
-      desc: '已适配星铁、崩三',
+      desc: '已部分适配星铁、崩三',
       enable: false,
+      actionCd: 1000,
+      actionMaximum: 3,
       rules: [
         {
           name: '崩坏：星穹铁道',
+          key: 1,
           action: 'clickCenter',
           activityIds: '.web2.MiHoYoWebActivity',
-          actionCdKey: 1,
           anyMatches: [
             'WebView[text*="星穹铁道"][text*="签到"] > View > View + View > View + View > View > @TextView[text*="第"][text*="天"]',
             'TextView[text*="级"] < View < View < View > @TextView[text*="第"][text*="天"]',
@@ -39,13 +41,27 @@ export default defineGkdApp({
         },
         {
           name: '崩坏3',
+          key: 3,
           action: 'clickCenter',
           activityIds: '.web2.MiHoYoWebActivity',
-          actionCdKey: 1,
           anyMatches: [
             'WebView[text*="崩坏3"][text*="签到"] > View > View + View > View > View + View > TextView[text*="第"][text*="天"] - @View',
             'Image[text="996923edce4749eff8536dbb75164c55_351516012348375859"] < @View',
           ],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '功能类-打卡',
+      desc: '全分区适配',
+      rules: [
+        {
+          fastQuery: true,
+          action: 'clickCenter',
+          activityIds: '.main.HyperionMainActivity',
+          matches:
+            'LinearLayout[vid="discussSignView"] TextView[vid="signTv"][text="已打卡"]',
         },
       ],
     },
