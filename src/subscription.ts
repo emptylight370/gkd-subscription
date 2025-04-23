d03779e4d7a007a8643ffd37031b6fdb80d69f3b
Emptylight <1378990254@qq.com>
2025-04-24 00:33:03 +0800

    Release version 27
    Changes(1): src/subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -6,7 +6,7 @@ import globalGroups from './globalGroups';
 export default defineGkdSubscription({
   id: 6676,
   name: "EmptyLight's Subscription",
-  version: 26,
+  version: 27,
   author: 'Emptylight',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/emptylight370/gkd-subscription/issues',
