fd42d526b4778b8a6b95935a7817d7ae6dc1b5a9
Emptylight <1378990254@qq.com>
2025-05-06 22:56:30 +0800

    feat: Simply Updates
    files(1): src/apps/com.oplus.ota.ts

---
+++ src/apps/com.oplus.ota.ts
@@ -0,0 +1,22 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.oplus.ota',
+  name: '软件更新',
+  groups: [
+    {
+      key: 1,
+      name: '功能弹窗-拒绝自动下载更新包',
+      enable: false,
+      rules: [
+        {
+          activityIds: 'com.oplus.otaui.activity.EntryActivity',
+          matches: [
+            '[vid="alertTitle"][text="开启自动下载"]',
+            '[text="关闭"] <<2 RelativeLayout',
+          ],
+        },
+      ],
+    },
+  ],
+});
