30c26ff5bc8603511277f21d232b2224190029b0
Emptylight <1378990254@qq.com>
2025-05-17 00:46:00 +0800

    feat: Simply Updates
    files(1): src/apps/kylec.me.lightbookkeeping.ts

---
+++ src/apps/kylec.me.lightbookkeeping.ts
@@ -0,0 +1,25 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'kylec.me.lightbookkeeping',
+  name: '一羽记账',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-自动同步',
+      desc: '在同步异常的时候自动点击同步',
+      enable: false,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.HomeActivity',
+          matches: [
+            '[vid="tvUnSyncBillCount"]',
+            '[vid="ivSyncRedNotice"]',
+            '[vid="ivBackupAndSync"]',
+          ],
+        },
+      ],
+    },
+  ],
+});
