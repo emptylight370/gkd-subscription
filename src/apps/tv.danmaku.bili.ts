4e8d677d0ef82aa70ef00502dac0ff6bde9e53e6
Emptylight <1378990254@qq.com>
2025-05-19 23:29:49 +0800

    feat: Simply Updates
    files(1): src/apps/tv.danmaku.bili.ts

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -65,5 +65,18 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 6,
+      name: '局部广告-直播间小广告',
+      enable: false,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds:
+            'com.bilibili.bililive.room.ui.roomv3.LiveRoomActivityV3',
+          matches: '[vid="live_game_card_close"]',
+        },
+      ],
+    },
   ],
 });
