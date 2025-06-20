a8a40cd429c7e7e5aa4411df09896b5181f861bc
Emptylight <1378990254@qq.com>
2025-06-20 12:40:39 +0800

    feat: Simply Updates
    files(1): src/apps/tv.danmaku.bili.ts

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -9,11 +9,19 @@ export default defineGkdApp({
       name: '局部广告-游戏中心添加至桌面横幅',
       enable: false,
       actionMaximum: 1,
+      fastQuery: true,
+      activityIds: 'com.bilibili.biligame.ui.GameCenterHomeActivity',
       rules: [
         {
-          fastQuery: true,
-          activityIds: 'com.bilibili.biligame.ui.GameCenterHomeActivity',
+          name: '关闭横幅',
+          key: 1,
           matches: '[vid="biligame_shortcut_close"]',
+        },
+        {
+          name: '关闭弹窗',
+          key: 2,
+          preKeys: [1],
+          matches: '[text="稍后添加"]',
         },
       ],
     },
