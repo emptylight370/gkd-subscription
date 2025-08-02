c504bbb70522a6a446c67abecd6a6b1fb19bb7f1
Emptylight <1378990254@qq.com>
2025-08-02 13:00:39 +0800

    feat: Simply Updates
    files(1): src/apps/com.greenpoint.android.mc10086.activity.ts

--- src/apps/com.greenpoint.android.mc10086.activity.ts
+++ src/apps/com.greenpoint.android.mc10086.activity.ts
@@ -46,5 +46,32 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 3,
+      name: '全屏广告-不使用中移支付',
+      enable: false,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.wxapi.WXPayEntryActivity',
+          matches: [
+            '[vid="pointKeep_dialog_title"][text="中移支付"]',
+            '[vid="discard_button"][text="放弃优惠"]',
+          ],
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '全屏广告-首页全屏广告',
+      enable: false,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.mc10086.cmcc.view.tabs.AppTabFragment',
+          matches: ['[text="广告"]', '[vid="iv_close_top"]'],
+        },
+      ],
+    },
   ],
 });
