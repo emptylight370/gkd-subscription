63b738998d8a46edd81d2521c0ab728b7a989414
Emptylight <1378990254@qq.com>
2026-01-26 14:26:45 +0800

    feat: Simply Updates
    files(1): com.coloros.phonemanager.ts

--- src/apps/com.coloros.phonemanager.ts
+++ src/apps/com.coloros.phonemanager.ts
@@ -16,5 +16,17 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '局部广告-添加桌面卡片',
+      enable: false,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.oplus.phonemanager.common.DialogCrossActivity',
+          matches: ['[vid="tv_title"][text="添加桌面卡片"]', '[vid="tv_cancel"][text="取消"]'],
+        },
+      ],
+    },
   ],
 });
