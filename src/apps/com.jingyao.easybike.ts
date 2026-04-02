dcf947e6c1898fc38560955f9b80262456d3ce0b
Emptylight <1378990254@qq.com>
2026-04-02 17:35:43 +0800

    feat: Simply Updates
    files(1): com.jingyao.easybike.ts

--- src/apps/com.jingyao.easybike.ts
+++ src/apps/com.jingyao.easybike.ts
@@ -7,12 +7,17 @@ export default defineGkdApp({
     {
       key: 1,
       name: '全屏广告-首页全屏广告',
+      fastQuery: true,
+      activityIds: 'com.hellobike.atlas.business.portal.PortalActivity',
       enable: false,
       rules: [
         {
-          fastQuery: true,
-          activityIds: 'com.hellobike.atlas.business.portal.PortalActivity',
+          name: '弹窗关闭按钮',
           matches: '[vid="actionDialogClose"]',
+        },
+        {
+          name: '欢迎来到地级市',
+          matches: '@ImageView < ViewGroup <2 ViewGroup + ViewGroup > ViewGroup > ViewGroup > [text^="你好,欢迎来到"]',
         },
       ],
     },
