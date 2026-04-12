e28c5acb821243f50238547a405b64be3394cc2b
Emptylight <1378990254@qq.com>
2026-04-13 01:41:04 +0800

    feat: Simply Updates
    files(1): com.huawei.health.ts

--- src/apps/com.huawei.health.ts
+++ src/apps/com.huawei.health.ts
@@ -8,11 +8,24 @@ export default defineGkdApp({
       key: 1,
       name: '局部广告-手环会员领取',
       enable: false,
+      actionMaximum: 1,
       rules: [
         {
           fastQuery: true,
           activityIds: '.MainActivity',
           matches: '[vid="user_content_vibrant_life"] +2 LinearLayout > TextView[text="取消"]',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '权限提示-设置后台保护',
+      enable: false,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.huawei.ui.homewear21.home.WearHomeOldActivity',
+          matches: ['[vid="toast_title_tv_bold"][text*="后台保护"]', '[vid="toast_cancel_tv"][text="忽略"]'],
         },
       ],
     },
