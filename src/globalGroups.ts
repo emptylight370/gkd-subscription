a69273d74bbc85c7b4d95381cabb517900d901e5
Emptylight <1378990254@qq.com>
2026-04-25 14:59:37 +0800

    feat: Simply Updates
    files(1): globalGroups.ts

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -9,6 +9,7 @@ import {
 export default defineGkdGlobalGroups([
   {
     // https://github.com/AIsouler/GKD_subscription/blob/main/src/globalGroups.ts
+    // https://github.com/Lin-arm/GKD_subscription/blob/main/src/globalGroups.ts
     key: 1,
     name: '开屏广告-全局',
     desc: '关闭打开应用时的开屏广告',
@@ -44,28 +45,30 @@ export default defineGkdGlobalGroups([
           'https://i.gkd.li/i/24766641', // height<300
 
           // 避免被excludeMatches匹配
-          'https://i.gkd.li/i/24330969',
-          'https://i.gkd.li/i/24541384',
-          'https://i.gkd.li/i/24588777',
+          'https://i.gkd.li/i/24330969', // [text="搜索运单或服务"] 加 text.length<7 后被排除
+          'https://i.gkd.li/i/24541384', // [text="设置"] 加 [visibleToUser=true] 后被排除
+          'https://i.gkd.li/i/24588777', // 原先 [text*="隐私协议"] 会匹配,已删除
         ],
         excludeSnapshotUrls: [
           // 避免误触
-          'https://i.gkd.li/i/15079224', // !(text*="退出")
-          'https://i.gkd.li/i/17108010', // !(text="帮助")
-          'https://i.gkd.li/i/18265000', // !(text="取消")
           'https://i.gkd.li/i/19952277', // text="Submit"
-          'https://i.gkd.li/i/20946730', // text$="设置"
-          'https://i.gkd.li/i/20949002', // vid!~="(?is).*video.*"
-          'https://i.gkd.li/i/21617520', // text!*="视频"
-          'https://i.gkd.li/i/22634992', // text$="登录" text*="阅读并同意"
+          'https://i.gkd.li/i/22634992', // text*="阅读并同意"
+          'https://i.gkd.li/i/23122415', // text="书签"
+          'https://i.gkd.li/i/23225609', // text="NEXT"
+          'https://i.gkd.li/i/20946730', // text$="设置" text*="跳过片"
+          'https://i.gkd.li/i/23052289', // text="选好了"
+          'https://i.gkd.li/i/23741801', // text^="下一步" text$="设置"
+          'https://i.gkd.li/i/23741779', // text^="完成" text$="设置"
           'https://i.gkd.li/i/23051921', // [text^="选择"]
           'https://i.gkd.li/i/23742770', // [text^="选择"]
           'https://i.gkd.li/i/23743049', // [text^="选择"]
-          'https://i.gkd.li/i/23052289', // text="选好了"
-          'https://i.gkd.li/i/23122415', // text="书签"
-          'https://i.gkd.li/i/23225609', // text="NEXT"
-          'https://i.gkd.li/i/23741801', // text^="下一步" text$="设置"
-          'https://i.gkd.li/i/23741779', // text^="完成" text$="设置"
+
+          // anyMatches 中的排除
+          'https://i.gkd.li/i/15079224', // !(text*="退出")
+          'https://i.gkd.li/i/17108010', // !(text="帮助")
+          'https://i.gkd.li/i/18265000', // !(text="取消")
+          'https://i.gkd.li/i/20949002', // vid!~="(?is).*video.*"
+          'https://i.gkd.li/i/21617520', // text!*="视频"
           'https://i.gkd.li/i/25039297', // text*="跳过片"、text!*="片头"、text!*="片尾"、vid!~="(?is).*head.*"、vid!~="(?is).*tail.*"
         ],
       },
@@ -91,6 +94,9 @@ export default defineGkdGlobalGroups([
       .concat([...openAdWhiteListAppIDs].map((id) => ({ id, enable: true }))),
   },
   {
+    // https://github.com/AIsouler/GKD_subscription/blob/main/src/globalGroups.ts
+    // https://github.com/Lin-arm/GKD_subscription/blob/main/src/globalGroups.ts
+    // https://github.com/ganlinte/GKD-subscription/blob/main/src/globalGroups.ts
     key: 2,
     name: '青少年模式-全局',
     desc: '关闭应用的青少年模式弹窗',
@@ -106,7 +112,10 @@ export default defineGkdGlobalGroups([
         name: 'AIsouler',
         matches: [
           '[text*="青少年" || text*="未成年" || text*="儿童"][text*="模式" || text*="守护"][text.length<15][childCount=0][visibleToUser=true]',
-          '[text*="知道了" || text*="我已知晓" || text*="已满" || text*="不再提醒"][text.length<8][childCount=0][visibleToUser=true]',
+          '[text*="知道了" || text*="我已知晓" || text*="已满" || text*="不再提醒" || text="关闭"][text.length<8][childCount=0][visibleToUser=true]',
+        ],
+        snapshotUrls: [
+          'https://i.gkd.li/i/23427881', // 关闭
         ],
       },
       {
