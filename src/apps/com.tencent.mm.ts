4c1eb51f5c0d85f23f05b3d001ea03ab255557f9
Emptylight <1378990254@qq.com>
2026-04-27 15:00:28 +0800

    feat: Simply Updates
    files(1): com.tencent.mm.ts

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -15,5 +15,26 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '开屏广告-小程序启动广告',
+      enable: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'match',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: [
+            '.plugin.appbrand.ui.AppBrandUI00',
+            '.plugin.appbrand.ui.AppBrandUI01',
+            '.plugin.appbrand.ui.AppBrandUI02',
+            '.plugin.appbrand.ui.AppBrandUI03',
+            '.plugin.appbrand.ui.AppBrandUI04',
+          ],
+          matches: ['[text="广告"]', '[text="秒"]', '[text="跳过"]'],
+        },
+      ],
+    },
   ],
 });
