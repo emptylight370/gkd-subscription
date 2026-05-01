8fe24d88528fe019638486043f84a94922868266
Emptylight <1378990254@qq.com>
2026-05-01 22:40:02 +0800

    feat: Simply Updates
    files(2): com.greenpoint.android.mc10086.activity.ts, com.sinovatech.unicom.ui.ts

--- src/apps/com.greenpoint.android.mc10086.activity.ts
+++ src/apps/com.greenpoint.android.mc10086.activity.ts
@@ -5,50 +5,6 @@ export default defineGkdApp({
   name: '中国移动',
   groups: [
     {
-      key: 1,
-      name: '分段功能-微信支付',
-      enable: false,
-      fastQuery: true,
-      activityIds: '.wxapi.WXPayEntryActivity',
-      actionMaximum: 2,
-      rules: [
-        {
-          key: 1,
-          name: '展开更多',
-          matches: '[vid="more_arraw"]',
-        },
-        {
-          key: 2,
-          preKeys: [1],
-          name: '点击微信支付',
-          matches:
-            '@[vid="paytype_check"][checked=false][visibleToUser=true] < * - * >2 [text="微信支付"][visibleToUser=true]',
-        },
-      ],
-    },
-    {
-      key: 2,
-      name: '分段功能-支付宝支付',
-      enable: false,
-      fastQuery: true,
-      activityIds: '.wxapi.WXPayEntryActivity',
-      actionMaximum: 2,
-      rules: [
-        {
-          key: 1,
-          name: '展开更多',
-          matches: '[vid="more_arraw"]',
-        },
-        {
-          key: 2,
-          preKeys: [1],
-          name: '点击支付宝支付',
-          matches:
-            '@[vid="paytype_check"][checked=false][visibleToUser=true] < * - * >2 [text="支付宝支付"][visibleToUser=true]',
-        },
-      ],
-    },
-    {
       key: 3,
       name: '全屏广告-不使用中移支付',
       enable: false,
--- src/apps/com.sinovatech.unicom.ui.ts
+++ src/apps/com.sinovatech.unicom.ui.ts
@@ -5,62 +5,6 @@ export default defineGkdApp({
   name: '中国联通',
   groups: [
     {
-      key: 1,
-      name: '分段功能-微信支付',
-      enable: false,
-      fastQuery: true,
-      activityIds: 'com.unicom.pay.normal.order.ui.WPOrderActivity',
-      rules: [
-        {
-          key: 1,
-          name: '展开更多',
-          matches: '[text="其他支付方式"]',
-        },
-        {
-          key: 2,
-          preKeys: [1],
-          name: '点击微信支付v1',
-          versionCode: { maximum: 182 },
-          matches: '@[vid="wopay_other_item_icon_font"] < * - * > [text="微信"]',
-        },
-        {
-          key: 3,
-          preKeys: [1],
-          name: '点击微信支付v2',
-          versionCode: { minimum: 183 },
-          matches: '@[vid="wopay_discount_right_ll"] - ViewGroup > ViewGroup > * > TextView[text="微信"]',
-        },
-      ],
-    },
-    {
-      key: 2,
-      name: '分段功能-支付宝支付',
-      enable: false,
-      fastQuery: true,
-      activityIds: 'com.unicom.pay.normal.order.ui.WPOrderActivity',
-      rules: [
-        {
-          key: 1,
-          name: '展开更多',
-          matches: '[text="其他支付方式"]',
-        },
-        {
-          key: 2,
-          preKeys: [1],
-          name: '点击支付宝支付v1',
-          versionCode: { maximum: 182 },
-          matches: '@[vid="wopay_other_item_icon_font"] < * - * > [text="支付宝"]',
-        },
-        {
-          key: 3,
-          preKeys: [1],
-          name: '点击支付宝支付v2',
-          versionCode: { minimum: 183 },
-          matches: '@[vid="wopay_discount_right_ll"] - ViewGroup > ViewGroup > * > TextView[text="支付宝"]',
-        },
-      ],
-    },
-    {
       key: 3,
       name: '全屏广告-若干广告',
       enable: false,
