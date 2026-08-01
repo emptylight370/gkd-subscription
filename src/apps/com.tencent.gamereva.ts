2c35d574d6040274fb972e4065a38bf0066d585a
Emptylight <1378990254@qq.com>
2026-08-01 16:29:49 +0800

    feat: Simply Updates
    files(1): com.tencent.gamereva.ts

--- src/apps/com.tencent.gamereva.ts
+++ src/apps/com.tencent.gamereva.ts
@@ -15,5 +15,21 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-首页广告',
+      desc: '类似于开屏广告',
+      enable: false,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.home.UfoHomeActivity',
+          matches: '[vid="id_btn_cancel"]',
+        },
+      ],
+    },
   ],
 });
