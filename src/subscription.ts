0e58ebb55b25e2200ec1193a6eda8d90c85da15e
Emptylight <1378990254@qq.com>
2026-08-01 16:42:43 +0800

    chore(version): Release new version
    Changes(1): subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -6,7 +6,7 @@ import globalGroups from './globalGroups';
 export default defineGkdSubscription({
   id: 6676,
   name: 'EmptyLight Subscription',
-  version: 140,
+  version: 141,
   author: 'Emptylight',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/emptylight370/gkd-subscription/issues',
